a7bac4b2d8269d089cef33e7cd1211968497253d
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-10-08 11:06:28 +0300

    bootstarp installd and create component interactions

---
+++ src/app/Components/Day4/ComponentsInteractions/registeration/registeration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registeration',
+  imports: [],
+  templateUrl: './registeration.html',
+  styleUrl: './registeration.css'
+})
+export class Registeration {
+
+}
